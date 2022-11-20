90904dc10fdd17ec56b1d593f2478fc85dc78b33
Ikraanaish <Ikraanaish@icloud.com>
2022-11-20 10:39:04 -0800

    created a readme,putchar.c and main.h

---
+++ README.md
@@ -0,0 +1,2 @@
+Group project to create a printf function similar to the printf in the stdio
+group members: Mariam Abdulkadir Mohammed and Aisha Abdulkadir mohammed
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdlib.h>
+#include<stdarg.h>
+/*write functions*/
+int _putchar(char c);
+/*printf*/
+int _printf(const char *format, ...);
+
+
+
+#endif
